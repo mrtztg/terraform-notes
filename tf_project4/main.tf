e5ff0fba4679e75aba73d26bd3d73eae1752ae75
Morteza <76879732+mrtztg@users.noreply.github.com>
2025-08-16 11:19:23 +0100

    add ami, vpc, subnets

--- tf_project4/main.tf
+++ tf_project4/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "morytfstate"
-    key          = "tf_project/tf.tfstate"
+    key          = "tf_project/tf4.fstate"
     region       = "eu-west-2"
     use_lockfile = true
   }
@@ -19,23 +19,6 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-resource "aws_instance" "tf4_ec2" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  tenancy = "default"
-
-  tags = {
-    purpose = "practice"
-  }
-}
-
-resource "aws_s3_bucket" "tf4_s3" {
-  bucket = "morytf4bucket"
-  tags = {
-    purpose = "practice"
-  }
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
   
@@ -45,4 +28,63 @@ data "aws_ami" "ubuntu" {
   }
 
   owners = [ "099720109477" ] # Canonical
+}
+
+resource "aws_instance" "tf4_ec2" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tenancy = "default"
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello World 1" > index.html
+            python3 -m http.server 8080 &
+            EOF
+
+  tags = {
+    purpose = "practice"
+  }
+}
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data
+
+resource "aws_instance" "tf4_ec2_2" {
+  ami = data.aws_ami.ubuntu.id
+  tenancy = "default"
+  instance_type = "t3.nano"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'Hello world 2" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
+
+resource "aws_s3_bucket" "tf4_s3" {
+  bucket = "morytf4bucket"
+  force_destroy = true
+  tags = {
+    purpose = "practice"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf4_s3_versioning" {
+  bucket = aws_s3_bucket.tf4_s3.id
+  
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf4_s3_encryption" {
+  bucket = aws_s3_bucket.tf4_s3.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
