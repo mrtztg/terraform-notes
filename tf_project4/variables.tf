c299dd1de15a6aa7b3ef163e976d6beb4e0536e2
Morteza <76879732+mrtztg@users.noreply.github.com>
2025-08-19 13:52:34 +0100

    use variables and outputs

--- tf_project4/variables.tf
+++ tf_project4/variables.tf
@@ -1,9 +1,54 @@
 variable "region" {
-  default = "eu-west-2"
-  type    = string
+  default     = "eu-west-2"
+  type        = string
+  nullable    = false
+  description = "Region of infra"
 }
 
 variable "aws_profile" {
-  default = "terraform"
+  default     = "terraform"
+  type        = string
+  description = "AWS CLI aws_profile to use. It should be defined before using here."
+}
+
+variable "ami_filter" {
+  type = object({
+    filter = string,
+    owner  = string
+  })
+  description = "AMI filter"
+}
+
+variable "ec2_instance_configs" {
+  type = list(object({
+    instance_type = string,
+    tenancy       = string
+  }))
+}
+
+variable "ec2_port" {
+  type        = number
+  default     = 8080
+  description = "Exposed ports of EC2 instances"
+}
+
+variable "s3_bucket_name" {
   type    = string
+  default = "Name of s3 bucket"
+}
+
+variable "db_config" {
+  type = object({
+    db_name           = string,
+    instance_type     = string,
+    allocated_storage = number
+  })
+}
+
+variable "db_credentials" {
+  sensitive = true
+  type = object({
+    username = string,
+    password = string
+  })
 }
